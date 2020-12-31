e5adaee270e6e9cdcb26f1419e11f4c45afca005
Hamza SAIH <hamzasaih.2a@gmail.com>
2020-12-31 21:42:21 +0000

    Scroll view

--- app/src/main/java/com/mzsaih/androidtutorial/CounterActivity.java
+++ app/src/main/java/com/mzsaih/androidtutorial/CounterActivity.java
@@ -3,12 +3,26 @@ package com.mzsaih.androidtutorial;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class CounterActivity extends AppCompatActivity {
-
+    int mCounter;
+    TextView tvCounter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_counter);
+        //initialisation
+        tvCounter = findViewById(R.id.txtcount);
+        mCounter = 0;
+    }
+
+    public void countUp(View view){
+        mCounter++;
+        Toast.makeText(CounterActivity.this,Integer.toString(mCounter),Toast.LENGTH_SHORT).show();
+        if (tvCounter != null)
+            tvCounter.setText(Integer.toString(mCounter));
     }
 }
