ebc66ad1958b8887e8467f6e20aff7841554392b
Hamza SAIH <hamzasaih.2a@gmail.com>
2021-01-06 00:16:15 +0000

    Formation 06/01 OOP

--- app/src/main/java/com/mzsaih/androidtutorial/TestClassActivity.java
+++ app/src/main/java/com/mzsaih/androidtutorial/TestClassActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Toast;
 
+
 public class TestClassActivity extends AppCompatActivity {
 
     @Override
