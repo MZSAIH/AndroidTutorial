44e1bfcd04d2075f837fe360089e0cda12d086a4
Hamza SAIH <hamzasaih.2a@gmail.com>
2021-01-07 23:59:35 +0000

    Custom list adapter - suite

--- app/src/main/java/com/mzsaih/androidtutorial/CarAdapter.java
+++ app/src/main/java/com/mzsaih/androidtutorial/CarAdapter.java
@@ -1,9 +1,11 @@
 package com.mzsaih.androidtutorial;
 
 import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 
@@ -32,7 +34,30 @@ public class CarAdapter extends BaseAdapter {
     }
 
     @Override
-    public View getView(int i, View view, ViewGroup viewGroup) {
-        return null;
+    public View getView(int position, View view, ViewGroup viewGroup) {
+
+        listItem item;
+        //Bind Java code with XML elements
+        if (view == null){
+            item = new listItem();
+            LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            view = inflater.inflate(R.layout.list_item_car, null, true);
+            item.tv_marque = view.findViewById(R.id.marque);
+            item.tv_hp     = view.findViewById(R.id.hp);
+            view.setTag(item);
+        }else{
+            item = (listItem) view.getTag();
+        }
+
+        //Populate listitem Class with data
+        item.tv_marque.setText(carlist.get(position).getMarque());
+        item.tv_hp.setText(String.valueOf(carlist.get(position).getHorsePower()));
+
+        return view;
+    }
+
+    class listItem{
+        TextView tv_marque;
+        TextView tv_hp;
     }
 }
